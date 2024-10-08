e7ecf53b4a3451ce34db2ff60f1bda1fe9149f4d
Sergey Mosin <sergey@srgdev.com>
2024-10-08 15:01:18 -0400

    bump version to v1.0.5

--- plugins/compact-composer/index.php
+++ plugins/compact-composer/index.php
@@ -6,8 +6,8 @@ class CompactComposerPlugin extends \RainLoop\Plugins\AbstractPlugin
 		NAME = 'Compact Composer',
 		AUTHOR = 'Sergey Mosin',
 		URL = 'https://github.com/the-djmaze/snappymail/pull/1466',
-		VERSION = '1.0.4',
-		RELEASE = '2024-04-23',
+		VERSION = '1.0.5',
+		RELEASE = '2024-08-08',
 		REQUIRED = '2.34.0',
 		LICENSE = 'AGPL v3',
 		DESCRIPTION = 'WYSIWYG editor with a compact toolbar';
